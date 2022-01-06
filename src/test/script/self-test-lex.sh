3a3b34094716b3db4a20212f5df1a8cba0ba4886
lecampt <titouan.lecamp@grenoble-inp.org>
2022-01-06 13:56:53 +0100

    Amélioration du test automatique du lexer

--- src/test/script/self-test-lex.sh
+++ src/test/script/self-test-lex.sh
@@ -11,66 +11,79 @@ PATH=./src/test/script/launchers:"$PATH"
 rouge='\e[31m'
 vert='\e[32m'
 blanc='\e[0;m'
+jaune='\e[33m'
 
 status=0
 
 
 # Gestion des cas valide
 
+echo -e "${jaune}Cas valide donné ${blanc}"
+
 for i in ./src/test/deca/syntax/valid/provided/*.deca
 do
-  if test_lex "$i" 2>&1 | grep -q -e 'Error'
+  fichier=$(basename $i)
+  if test_lex "$i" 2>&1 | grep -q -e "$fichier"
   then
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${rouge} FAILED ${blanc} (failure)"
+    echo -e "$fichier ${rouge} FAILED ${blanc} (failure)"
     tail -2 $i
     status=1
   else
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${vert} PASSED ${blanc}"
+    echo -e "$fichier ${vert} PASSED ${blanc}"
 
   fi
 done
 
+echo -e "${jaune}Cas valide créé ${blanc}"
+
 for i in ./src/test/deca/syntax/valid/self/*.deca
 do
-  if test_lex "$i" 2>&1 | grep -q -e 'Error'
+  fichier=$(basename $i)
+  if test_lex "$i" 2>&1 | grep -q -e "$fichier"
   then
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${rouge} FAILED ${blanc} (failure)"
+    echo -e "$fichier ${rouge} FAILED ${blanc} (failure)"
     tail -2 $i
     status=1
   else
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${vert} PASSED ${blanc}"
+    echo -e "$fichier ${vert} PASSED ${blanc}"
 
   fi
 done
 
 #Gestion des cas non valide
 
+echo -e "${jaune}Cas non valide donné ${blanc}"
+
 for i in ./src/test/deca/syntax/invalid/provided/*.deca
 do
-  if test_lex "$i" 2>&1 | grep -q -e 'Error'
+  fichier=$(basename $i)
+  if test_lex "$i" 2>&1 | grep -q -e "$fichier"
   then
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${vert} PASSED ${blanc} (failure)"
+    echo -e "$fichier ${vert} PASSED ${blanc} (failure)"
   else
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${rouge} FAILED ${blanc} (no failure)"
+    echo -e "$fichier ${rouge} FAILED ${blanc} (no failure)"
     status=1
   fi
 done
 
+echo -e "${jaune}Cas non valide créé ${blanc}"
+
 for i in ./src/test/deca/syntax/invalid/self/*.deca
 do
-  if test_lex "$i" 2>&1 | grep -q -e 'Error'
+  fichier=$(basename $i)
+  if test_lex "$i" 2>&1 | grep -q -e "$fichier"
   then
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${vert} PASSED ${blanc} (failure)"
+    echo -e "$fichier ${vert} PASSED ${blanc} (failure)"
   else
     test_lex "$i" >& "${i%.deca}".lis
-    echo -e "$i ${rouge} FAILED ${blanc} (no failure)"
+    echo -e "$fichier ${rouge} FAILED ${blanc} (no failure)"
     status=1
   fi
 done
