20bc659f38337a8f63926b39921dca3255c2eafb
Romain Pigret-Cadou <romain.pigret-cadou@grenoble-inp.org>
2022-01-07 18:35:41 +0100

    Minor changes and todo list

--- src/main/java/fr/ensimag/deca/CompilerOptions.java
+++ src/main/java/fr/ensimag/deca/CompilerOptions.java
@@ -1,15 +1,10 @@
 package fr.ensimag.deca;
 
 import java.io.File;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
-import fr.ensimag.deca.syntax.DecaLexer;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -21,9 +16,10 @@ import org.apache.log4j.Logger;
  */
 public class CompilerOptions {
     public static final int QUIET = 0;
-    public static final int INFO  = 1;
+    public static final int INFO = 1;
     public static final int DEBUG = 2;
     public static final int TRACE = 3;
+
     public int getDebug() {
         return debug;
     }
@@ -35,43 +31,47 @@ public class CompilerOptions {
     public boolean getPrintBanner() {
         return printBanner;
     }
-    
+
     public List<File> getSourceFiles() {
         return Collections.unmodifiableList(sourceFiles);
     }
 
-    public boolean getParser(){return parser;}
+    public boolean getParser() {
+        return parser;
+    }
 
-    public boolean getVerif(){return verif;}
+    public boolean getVerif() {
+        return verif;
+    }
 
     private int debug = 0;
     private boolean parallel = false;
     private boolean printBanner = false;
     private List<File> sourceFiles = new ArrayList<File>();
-    private boolean parser=false;
-    private boolean verif=false;
-
+    private boolean parser = false;
+    private boolean verif = false;
 
     public void parseArgs(String[] args) throws CLIException {
         // A FAIRE : parcourir args pour positionner les options correctement.
-        for (String arg : args){
-            switch (arg){
+        for (String arg : args) {
+            // A FAIRE : exclure -v et -p
+            switch (arg) {
                 case "-b":
-                    printBanner=true;
+                    printBanner = true;
                     break;
                 case "-p":
-                    parser=true;
+                    parser = true;
                     break;
                 case "-v":
-                    verif=true;
+                    verif = true;
                     break;
                 case "-n":
-                    //des trucs
+                    // A FAIRE
                     break;
-                case "-r":
-                case "-d":
+                case "-r": // A FAIRE
+                case "-d": // A FAIRE
                 case "-P":
-                    parallel=true;
+                    parallel = true;
                     break;
                 default:
                     sourceFiles.add(new File(arg));
@@ -80,15 +80,20 @@ public class CompilerOptions {
         Logger logger = Logger.getRootLogger();
         // map command-line debug option to log4j's level.
         switch (getDebug()) {
-        case QUIET: break; // keep default
-        case INFO:
-            logger.setLevel(Level.INFO); break;
-        case DEBUG:
-            logger.setLevel(Level.DEBUG); break;
-        case TRACE:
-            logger.setLevel(Level.TRACE); break;
-        default:
-            logger.setLevel(Level.ALL); break;
+            case QUIET:
+                break; // keep default
+            case INFO:
+                logger.setLevel(Level.INFO);
+                break;
+            case DEBUG:
+                logger.setLevel(Level.DEBUG);
+                break;
+            case TRACE:
+                logger.setLevel(Level.TRACE);
+                break;
+            default:
+                logger.setLevel(Level.ALL);
+                break;
         }
         logger.info("Application-wide trace level set to " + logger.getLevel());
 
@@ -100,10 +105,28 @@ public class CompilerOptions {
             logger.info("Java assertions disabled");
         }
 
-
     }
 
     protected void displayUsage() {
-        throw new UnsupportedOperationException("not yet implemented");
+        System.out.println("Utilisation : decac [OPTION] [FILE]");
+        System.out.println("Options disponibles :");
+        System.out.println(". -b (banner) : affiche une bannière indiquant le nom de l'équipe\n" +
+                ". -p (parse) : arrête decac après l'étape de construction de" +
+                "l'arbre, et affiche la décompilation de ce dernier" +
+                "(i.e. s'il n'y a qu'un fichier source à" +
+                "compiler, la sortie doit être un programme" +
+                "deca syntaxiquement correct)\n" +
+                ". -v (verification) : arrête decac après l'étape de vérifications" +
+                "(ne produit aucune sortie en l'absence d'erreur)\n" +
+                ". -n (no check) : supprime les tests à l'exécution spécifiés dans" +
+                "les points 11.1 et 11.3 de la sémantique de Deca.\n" +
+                ". -r X (registers) : limite les registres banalisés disponibles à" +
+                "R0 ... R{X-1}, avec 4 <= X <= 16\n" +
+                ". -d (debug) : active les traces de debug. Répéter" +
+                "l'option plusieurs fois pour avoir plus de" +
+                "traces.\n" +
+                ". -P (parallel) : s'il y a plusieurs fichiers sources," +
+                "lance la compilation des fichiers en" +
+                "parallèle (pour accélérer la compilation)");
     }
 }
--- src/main/java/fr/ensimag/deca/DecacMain.java
+++ src/main/java/fr/ensimag/deca/DecacMain.java
@@ -17,11 +17,10 @@ import org.apache.log4j.Logger;
  */
 public class DecacMain {
     private static Logger LOG = Logger.getLogger(DecacMain.class);
-    
+
     public static void main(String[] args) {
         // example log4j message.
         LOG.info("Decac compiler started");
-        System.out.println("Bienvenue dans Decac :");
         boolean error = false;
         final CompilerOptions options = new CompilerOptions();
         try {
@@ -33,43 +32,25 @@ public class DecacMain {
             System.exit(1);
         }
         if (options.getPrintBanner()) {
-            System.out.println("Le nom de l'équipe !");
-            System.out.println("By the way c'est l'équipe 13");
+            System.out.println("Nom de l'équipe : 13");
         }
-        if (options.getParser()){
+        if (options.getParser()) {
             System.out.println("Décompilation de l'arbre :");
-            for (File f : options.getSourceFiles()){
+            for (File f : options.getSourceFiles()) {
                 DecaLexer lexer = new DecaLexer((CharStream) f);
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 DecaParser parser = new DecaParser(tokens);
                 System.out.println(parser);
             }
-            //des trucs
+            // A FAIRE factorize code and stop compilation
         }
-        if (options.getVerif()){
+        if (options.getVerif()) {
             System.out.println("S'arrete après l'étape de vérification :");
-            //d'autres trucs
+            // A FAIRE
         }
         if (options.getSourceFiles().isEmpty()) {
-            System.out.println("--------------------------------------");
-            System.out.println(". -b (banner) : affiche une bannière indiquant le nom de l'équipe\n" +
-                    ". -p (parse) : arrête decac après l'étape de construction de" +
-                    "l'arbre, et affiche la décompilation de ce dernier" +
-                    "(i.e. s'il n'y a qu'un fichier source à" +
-                    "compiler, la sortie doit être un programme" +
-                    "deca syntaxiquement correct)\n" +
-                    ". -v (verification) : arrête decac après l'étape de vérifications" +
-                    "(ne produit aucune sortie en l'absence d'erreur)\n" +
-                    ". -n (no check) : supprime les tests à l'exécution spécifiés dans" +
-                    "les points 11.1 et 11.3 de la sémantique de Deca.\n" +
-                    ". -r X (registers) : limite les registres banalisés disponibles à" +
-                    "R0 ... R{X-1}, avec 4 <= X <= 16\n" +
-                    ". -d (debug) : active les traces de debug. Répéter" +
-                    "l'option plusieurs fois pour avoir plus de" +
-                    "traces.\n" +
-                    ". -P (parallel) : s'il y a plusieurs fichiers sources," +
-                    "lance la compilation des fichiers en" +
-                    "parallèle (pour accélérer la compilation)");
+            options.displayUsage();
+            System.exit(0);
         }
         if (options.getParallel()) {
             // A FAIRE : instancier DecacCompiler pour chaque fichier à
