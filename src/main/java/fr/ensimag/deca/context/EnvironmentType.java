7ca5e3534e5066b8d8cf0215e43d0aab3ea2d8ed
Romain Pigret-Cadou <romain.pigret-cadou@grenoble-inp.org>
2022-01-10 20:26:03 +0100

    📝doc in EnvironmenType

--- src/main/java/fr/ensimag/deca/context/EnvironmentType.java
+++ src/main/java/fr/ensimag/deca/context/EnvironmentType.java
@@ -4,6 +4,24 @@ import java.util.HashMap;
 
 import fr.ensimag.deca.tools.SymbolTable.Symbol;
 
+/**
+ * Dictionary associating identifier's TypeDefinition to their names.
+ * 
+ * This is actually a linked list of dictionaries: each EnvironmentType has a
+ * pointer to a parentEnvironment, corresponding to superblock (eg superclass).
+ * 
+ * The dictionary at the head of this list thus corresponds to the "current"
+ * block (eg class).
+ * 
+ * Searching a definition (through method get) is done in the "current"
+ * dictionary and in the parentEnvironment if it fails.
+ * 
+ * Insertion (through method declare) is always done in the "current"
+ * dictionary.
+ * 
+ * @author gl13
+ * @date 10/01/2022
+ */
 public class EnvironmentType {
 
     private HashMap<Symbol, TypeDefinition> environment;
@@ -19,6 +37,10 @@ public class EnvironmentType {
         private static final long serialVersionUID = -2733379901827316441L;
     }
 
+    /**
+     * Return the definition of the symbol in the environment, or null if the
+     * symbol is undefined.
+     */
     public TypeDefinition get(Symbol key) {
         TypeDefinition result = environment.get(key); // first look in current block
         if (result != null || parentEnvironment == null)
@@ -26,6 +48,22 @@ public class EnvironmentType {
         return parentEnvironment.get(key);
     }
 
+    /**
+     * Add the definition def associated to the symbol name in the environment.
+     * 
+     * Adding a symbol which is already defined in the environment,
+     * - throws DoubleDefException if the symbol is in the "current" dictionary
+     * - or, hides the previous declaration otherwise.
+     * 
+     * @param name
+     *             Name of the symbol to define
+     * @param def
+     *             Definition of the symbol
+     * @throws DoubleDefException
+     *                            if the symbol is already defined at the "current"
+     *                            dictionary
+     *
+     */
     public void declare(Symbol name, TypeDefinition def) throws DoubleDefException {
         // symbols are unique
         if (environment.containsKey(name))
