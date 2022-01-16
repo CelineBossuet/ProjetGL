eb7e5dd9d2ec213af9160d7499074426000340ce
Ayoub Miguil <ayoub.miguil@grenoble-inp.org>
2022-01-16 16:40:59 +0100

    astore and aload

--- src/main/java/fr/ensimag/bytecode/pseudocode/instructions/aload.java
+++ src/main/java/fr/ensimag/bytecode/pseudocode/instructions/aload.java
@@ -1,18 +1,11 @@
 package fr.ensimag.bytecode.pseudocode.instructions;
 
-import fr.ensimag.bytecode.pseudocode.BinaryInstructionDValToReg;
-import fr.ensimag.bytecode.pseudocode.DVal;
-import fr.ensimag.bytecode.pseudocode.GPRegister;
-import fr.ensimag.bytecode.pseudocode.ImmediateInteger;
+import fr.ensimag.bytecode.pseudocode.*;
 
-public class aload extends BinaryInstructionDValToReg {
+public class aload extends UnaryInstructionToReg {
 
-    public aload(DVal op1, GPRegister op2) {
-        super(op1, op2);
-    }
-
-    public aload(int i, GPRegister r) {
-        this(new ImmediateInteger(i), r);
+    public aload(GPRegister op) {
+        super(op);
     }
 
 }
---
+++ src/main/java/fr/ensimag/bytecode/pseudocode/instructions/astore.java
@@ -0,0 +1,10 @@
+package fr.ensimag.bytecode.pseudocode.instructions;
+
+import fr.ensimag.bytecode.pseudocode.GPRegister;
+import fr.ensimag.bytecode.pseudocode.UnaryInstructionToReg;
+
+public class astore extends UnaryInstructionToReg {
+
+    public astore(GPRegister op){super(op);}
+    
+}
