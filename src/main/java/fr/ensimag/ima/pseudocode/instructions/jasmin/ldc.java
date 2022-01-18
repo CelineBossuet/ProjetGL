d6a038a0aa54f262eb9fecc35189af6010ccd7d8
Ayoub Miguil <ayoub.miguil@grenoble-inp.org>
2022-01-18 02:04:57 +0100

    ldc

---
+++ src/main/java/fr/ensimag/ima/pseudocode/instructions/jasmin/ldc.java
@@ -0,0 +1,11 @@
+package fr.ensimag.ima.pseudocode.instructions.jasmin;
+
+import fr.ensimag.ima.pseudocode.Operand;
+import fr.ensimag.ima.pseudocode.UnaryInstruction;
+
+public class ldc extends UnaryInstruction {
+
+    protected ldc(Operand operand) {
+        super(operand);
+    }
+}
