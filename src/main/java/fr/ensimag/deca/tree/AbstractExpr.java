31f90169d8e33d876206e967b7634726a136ef72
bossuetc <bossuet.celine@grenoble-inp.org>
2022-01-22 15:39:39 +0100

    erreur sous typage pour classe

--- src/main/java/fr/ensimag/deca/tree/AbstractExpr.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractExpr.java
@@ -94,7 +94,9 @@ public abstract class AbstractExpr extends AbstractInst {
         Type type = this.verifyExpr(compiler, localEnv, currentClass);
         if (type.sameType(expectedType)) {
             if(type.isClass()){
-                if((((ClassType)type).isSubClassOf((ClassType) expectedType))){
+                ClassType classType = (ClassType)type;
+                ClassType expectedClassType = (ClassType) expectedType;
+                if(!(classType.isSubClassOf(expectedClassType))){
                     System.out.println("b");
                     throw new ContextualError(type.getName().getName()+" isn't a subtype of "
                             +expectedType.getName().getName()+" so it cannot be assigned to it", this.getLocation());
